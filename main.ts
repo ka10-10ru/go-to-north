d85832602b3f19cb7c61e6e9af6f10c8e319e71a
ka10-10ru <96565295+ka10-10ru@users.noreply.github.com>
2022-09-09 15:56:28 +0900

    added sonner

--- main.ts
+++ main.ts
@@ -1,28 +1,48 @@
 input.onButtonPressed(Button.A, function () {
     music.ringTone(northNoise)
 })
-input.onButtonPressed(Button.B, function () {
-	
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    input.calibrateCompass()
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    iFlag = 1
+    basic.showNumber(north)
+    basic.pause(2000)
+    iFlag = 0
+})
+function sonner () {
+    music.playTone(932, music.beat(BeatFraction.Whole))
+    basic.pause(noise - northNoise)
+}
 let northNoise = 0
-let noise = 2000
+let iFlag = 0
 let north = 0
+let noise = 0
+noise = 500
+north = 0
+iFlag = 0
 input.calibrateCompass()
 basic.forever(function () {
-    north = input.compassHeading()
-    if (north <= 180) {
-        northNoise = noise - noise / 180 * north
-    } else {
-        northNoise = noise / 180 * (north - noise)
+    if (iFlag == 0) {
+        north = input.compassHeading()
+        if (north <= 180) {
+            northNoise = noise - noise / 180 * north
+        } else {
+            northNoise = noise / 180 * north - noise
+        }
+        led.plotBarGraph(
+        northNoise,
+        noise
+        )
     }
-    led.plotBarGraph(
-    northNoise,
-    noise
-    )
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        music.ringTone(northNoise)
+        sonner()
     } else {
         music.stopAllSounds()
     }
